7395444a6b000defe8cc40bf0934b977dd6183e7
Alex Plasencia <aPlasencia895@hotmail.com>
2022-11-17 21:29:54 +0000

    Tested out more ESNext features.

--- index.ts
+++ index.ts
@@ -4,3 +4,5 @@ console.log(`This isn't training.  I'm only destroying nature.`);
 async function hello() {
   return 'world';
 }
+const alex = "my name is alex";
+console.log(`Testing out ${alex} more ESNext features on typescript`);
